3e44e3abc99c8d3d468b425611c0bc0104496f3d
Shuxiang Cao <shuxiang.cao@physics.ox.ac.uk>
2023-10-11 16:04:26 +0100

    func: Automatically log the logged uuid for the entry

--- labchronicle/decorators.py
+++ labchronicle/decorators.py
@@ -103,7 +103,8 @@ def _log_and_record(func, args, kwargs, record_details=True):
         if record_details:
             record.record_object(self)
 
-        self.logger.info(f'{record.record_id}: {func.__qualname__} recorded.')
+        # Could be too detailed. Comment out for now.
+        # self.logger.info(f'{record.record_id}: {func.__qualname__} recorded.')
 
     return retval
 
